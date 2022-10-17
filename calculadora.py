a16d1dabbc0b07d81669247c56c0d55866d7c370
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-10-17 11:07:16 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,6 @@
 class Calculadora():
     
-    def soma(numero1, numero2):
-        return numero1+numero2
-
-    def subtracao(numero1, numero2):
-        return numero1-numero2
-
-    def multiplicacao(numero1, numero2):
-        return numero1*numero2
-
-    def divisao(numero1, numero2):
-        if(numero2 == 0):
-            print("Não existe divisão por zero.")
-            return None
-        else:
-            return numero1/numero2
-        
-    if __name__ == '__main__':
+    def __init__(self):
         numero1 = int(input("Digite o primeiro número da operação: "))
         print("#"*40)
         numero2 = int(input("Digite o segundo número da operação: "))
@@ -24,13 +8,32 @@ class Calculadora():
         operacao = int(input("Agora escolha a operação desejada: 1 - Soma; 2 - Subtração; 3 - Multiplicação; 4 - Dvisão \n"))
         print("#"*90)
         if operacao == 1:
-            print("A soma dos números é igual a", soma(numero1, numero2))
+            print("A soma dos números é igual a", self.soma(numero1, numero2))
         elif operacao == 2:
-            print("A subtração dos números é igual a", subtracao(numero1, numero2))
+            print("A subtração dos números é igual a", self.subtracao(numero1, numero2))
         elif operacao == 3:
-            print("A multiplicação dos números é igual a", multiplicacao(numero1, numero2))
+            print("A multiplicação dos números é igual a", self.multiplicacao(numero1, numero2))
         elif operacao == 4:
-            print("A divisão dos números é igual a", divisao(numero1, numero2))
+            print("A divisão dos números é igual a", self.divisao(numero1, numero2))
         else:
             print("Opção inválida.")
             exit()
+            
+    def soma(self, numero1, numero2):
+        return numero1+numero2
+
+    def subtracao(self, numero1, numero2):
+        return numero1-numero2
+
+    def multiplicacao(self, numero1, numero2):
+        return numero1*numero2
+
+    def divisao(self, numero1, numero2):
+        if(numero2 == 0):
+            print("Não existe divisão por zero.")
+            return None
+        else:
+            return numero1/numero2
+        
+if __name__ == '__main__':
+    Calculadora()
