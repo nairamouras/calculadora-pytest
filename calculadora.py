39efd7de909830f21537125f9135f87af5362430
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-10-17 10:05:23 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,36 +1,36 @@
-def soma(numero1, numero2):
-    return numero1+numero2
+class Calculadora():
 
-def subtracao(numero1, numero2):
-    return numero1-numero2
+    def soma(numero1, numero2):
+        return numero1+numero2
 
-def multiplicacao(numero1, numero2):
-    return numero1*numero2
+    def subtracao(numero1, numero2):
+        return numero1-numero2
 
-def divisao(numero1, numero2):
-    if(numero2 == 0):
-        print("Não existe divisão por zero.")
-        exit()
-    else:
-        return numero1/numero2
-    
-def calculadora():
-    numero1 = int(input("Digite o primeiro número da operação: "))
-    print("#"*40)
-    numero2 = int(input("Digite o segundo número da operação: "))
-    print("#"*40)
-    operacao = int(input("Agora escolha a operação desejada: 1 - Soma; 2 - Subtração; 3 - Multiplicação; 4 - Dvisão \n"))
-    print("#"*80)
-    if operacao == 1:
-        print("A soma dos números é igual a", soma(numero1, numero2))
-    elif operacao == 2:
-        print("A subtração dos números é igual a", subtracao(numero1, numero2))
-    elif operacao == 3:
-        print("A multiplicação dos números é igual a", multiplicacao(numero1, numero2))
-    elif operacao == 4:
-        print("A divisão dos números é igual a", divisao(numero1, numero2))
-    else:
-        print("Opção inválida.")
-        exit()
+    def multiplicacao(numero1, numero2):
+        return numero1*numero2
 
-calculadora()
+    def divisao(numero1, numero2):
+        if(numero2 == 0):
+            print("Não existe divisão por zero.")
+            exit()
+        else:
+            return numero1/numero2
+        
+    if __name__ == '__main__':
+        numero1 = int(input("Digite o primeiro número da operação: "))
+        print("#"*40)
+        numero2 = int(input("Digite o segundo número da operação: "))
+        print("#"*40)
+        operacao = int(input("Agora escolha a operação desejada: 1 - Soma; 2 - Subtração; 3 - Multiplicação; 4 - Dvisão \n"))
+        print("#"*90)
+        if operacao == 1:
+            print("A soma dos números é igual a", soma(numero1, numero2))
+        elif operacao == 2:
+            print("A subtração dos números é igual a", subtracao(numero1, numero2))
+        elif operacao == 3:
+            print("A multiplicação dos números é igual a", multiplicacao(numero1, numero2))
+        elif operacao == 4:
+            print("A divisão dos números é igual a", divisao(numero1, numero2))
+        else:
+            print("Opção inválida.")
+            exit()
